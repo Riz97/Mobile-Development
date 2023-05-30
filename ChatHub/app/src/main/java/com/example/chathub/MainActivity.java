ce921679226729fc1d6e4aed871a790d5de05e16
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2023-05-30 14:09:38 +0200

    Login and Sign In xml

--- ChatHub/app/src/main/java/com/example/chathub/MainActivity.java
+++ ChatHub/app/src/main/java/com/example/chathub/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.chathub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 // Login window
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Click Listener Sign In button:
+        Button signinButton = (Button)findViewById(R.id.buttonSignin);
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signinIntent = new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(signinIntent);
+            }
+        });
     }
+
+
 }
