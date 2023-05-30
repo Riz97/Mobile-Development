4f2499b116b7f4cd93cc9ea6afe5063723e0da77
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2023-05-30 22:56:57 +0200

    .

--- ChatHub/app/src/main/java/com/example/chathub/SigninActivity.java
+++ ChatHub/app/src/main/java/com/example/chathub/SigninActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,6 +19,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class SigninActivity extends AppCompatActivity {
@@ -30,7 +33,7 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-
+        List<String> usernames = new ArrayList<String>();
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://chathub-caprile-benvenuto-default-rtdb.europe-west1.firebasedatabase.app/");
         databaseReference = database.getReference("Users");
 
@@ -39,42 +42,68 @@ public class SigninActivity extends AppCompatActivity {
         EditText editPassword = (EditText) (findViewById(R.id.editTextPassword));
         EditText editConfirm = (EditText) (findViewById(R.id.editTextConfirmPassword));
         Button signIn = (Button) findViewById(R.id.buttonSignin);
+        final String userEnteredUsername = editUsername.getText().toString().trim();
 
-
-
-
-        signIn.setOnClickListener(new View.OnClickListener() {
+        Query checkUser = databaseReference.orderByChild("username").equalTo(userEnteredUsername);
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d(LOG_TAG,"sono qua");
 
 
-                String password = editPassword.getText().toString();
-                String username = editUsername.getText().toString();
-                String confirmPsw = editConfirm.getText().toString();
+                for(DataSnapshot ds : snapshot.getChildren()) {
+                    String usernameFromDB = ds.child(userEnteredUsername).child("username").getValue(String.class);
+                    usernames.add(usernameFromDB);
 
-
-
-
-                if(password.equals(confirmPsw))
-                {
-                    newUser = new User(username,password);
-                    databaseReference.child(username).child("password").setValue(password);
-                    databaseReference.child(username).child("username").setValue(username);
-                    Toast.makeText(SigninActivity.this,"Profile Created Successfully",Toast.LENGTH_LONG).show();
+                    Log.d(LOG_TAG,usernames.toString());
                 }
-
-                else {
-
-
-                    Toast.makeText(SigninActivity.this," Error : Inserted Password do not correspond",Toast.LENGTH_LONG).show();
-
-
-                }
-
 
 
             }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
+
+
+                signIn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+
+                        String password = editPassword.getText().toString();
+                        String username = editUsername.getText().toString();
+                        String confirmPsw = editConfirm.getText().toString();
+
+                        if(usernames.contains(username))
+                        {
+                            editUsername.setError("User already Exists");
+                            editUsername.requestFocus();
+
+                        }
+
+
+
+                       else if (password.equals(confirmPsw) && !usernames.contains(username)) {
+                            newUser = new User(username, password);
+                            databaseReference.child(username).child("password").setValue(password);
+                            databaseReference.child(username).child("username").setValue(username);
+                            Toast.makeText(SigninActivity.this, "Profile Created Successfully", Toast.LENGTH_LONG).show();
+                        } else {
+
+
+                            Toast.makeText(SigninActivity.this, " Error : Inserted Password do not correspond", Toast.LENGTH_LONG).show();
+
+
+                        }
+
+
+
+
+                    }
+                });
     }
 
 
