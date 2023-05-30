6402645255c5ae80600ef62651612be6be5980da
Riz97 <93477707+Riz97@users.noreply.github.com>
2023-05-30 17:29:14 +0200

    .

--- ChatHub/app/src/main/java/com/example/chathub/MainActivity.java
+++ ChatHub/app/src/main/java/com/example/chathub/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.chathub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-
-
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 
 // Login window
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    DatabaseReference databaseReference;
 
+    User newUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://chathub-caprile-benvenuto-default-rtdb.europe-west1.firebasedatabase.app/");
+        databaseReference = database.getReference("Users");
+
+
+        EditText editUsername = (EditText) (findViewById(R.id.editTextUsername));
+        EditText editPassword = (EditText) (findViewById(R.id.editTextPassword));
+
 
 
         // Click Listener Sign In button:
@@ -39,13 +53,42 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance("https://chathub-caprile-benvenuto-default-rtdb.europe-west1.firebasedatabase.app/");
-                DatabaseReference myRef = database.getReference("message");
 
-                myRef.setValue("Hello, World!");
+
+                final String userEnteredUsername = editUsername.getText().toString().trim();
+                final String userEnteredPassword = editPassword.getText().toString().trim();
+                DatabaseReference reference = FirebaseDatabase.getInstance("https://chathub-caprile-benvenuto-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Users");
+                Log.d(LOG_TAG,reference.child("/").toString());
+                Query checkUser = reference.child("/").equalTo(userEnteredUsername);
+                checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            String passwordFromDB = dataSnapshot.child(userEnteredUsername).child("password").getValue(String.class);
+                            if (passwordFromDB.equals(userEnteredPassword)) {
+                                Intent signinIntent = new Intent(MainActivity.this, ChatActivity.class);
+                                startActivity(signinIntent);
+                            } else {
+                                editPassword.setError("Wrong Password");
+                                editPassword.requestFocus();
+                            }
+                        } else {
+                            editUsername.setError("No such User exist");
+                            editUsername.requestFocus();
+                        }
+
+
+
+
             }
-        });
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
 
-}
+});
+    }}
--- ChatHub/app/src/main/java/com/example/chathub/SigninActivity.java
+++ ChatHub/app/src/main/java/com/example/chathub/SigninActivity.java
@@ -2,13 +2,71 @@ package com.example.chathub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SigninActivity extends AppCompatActivity {
+    private static final String LOG_TAG = SigninActivity.class.getSimpleName();
 
+    DatabaseReference databaseReference;
+
+    User newUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://chathub-caprile-benvenuto-default-rtdb.europe-west1.firebasedatabase.app/");
+        databaseReference = database.getReference("Users");
+
+
+        EditText editUsername = (EditText) (findViewById(R.id.editTextUsername));
+        EditText editPassword = (EditText) (findViewById(R.id.editTextPassword));
+        EditText editConfirm = (EditText) (findViewById(R.id.editTextConfirmPassword));
+        Button signIn = (Button) findViewById(R.id.buttonSignin);
+
+
+
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String password = editPassword.getText().toString();
+                String username = editUsername.getText().toString();
+                String confirmPsw = editConfirm.getText().toString();
+
+                if(password.equals(confirmPsw))
+                {
+                    newUser = new User(username,password);
+                    databaseReference.child(username).child("password").setValue(password);
+                    Toast.makeText(SigninActivity.this,"Profile Created Successfully",Toast.LENGTH_LONG).show();
+                }
+
+                else {
+
+
+                    Toast.makeText(SigninActivity.this," Error : Inserted Password do not correspond",Toast.LENGTH_LONG).show();
+
+                    Log.d(LOG_TAG,"sono qua");
+                }
+
+
+
+            }
+        });
     }
+
+
+
+
 }
