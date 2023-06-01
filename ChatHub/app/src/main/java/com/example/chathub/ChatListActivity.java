0f0b5dcfdb36a4fe8f2d53931ea96ec0407fb90f
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2023-06-01 11:04:40 +0200

    Chat List

--- ChatHub/app/src/main/java/com/example/chathub/ChatListActivity.java
+++ ChatHub/app/src/main/java/com/example/chathub/ChatListActivity.java
@@ -27,6 +27,8 @@ public class ChatListActivity extends AppCompatActivity {
         String sharedPrefFile = "com.example.chathub";
         mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
 
+
+        /*
         // Click Listener for the "New Chat" button
         Button newChatButton = (Button) findViewById(R.id.buttonNewChat);
         newChatButton.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +52,7 @@ public class ChatListActivity extends AppCompatActivity {
                 preferencesEditor.clear();
                 preferencesEditor.apply();
             }
-        });
+        }); */
 
 
         int logged = mPreferences.getInt("logged", 0);
